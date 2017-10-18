efb61e64e863017ef1eb20400a43c5c8145649fd
sqlmfgjkql <ahmed.sayadi@etu.umontpellier.fr>
2017-10-18 17:09:26 +0200

    test

--- src/entities/Test.java
+++ src/entities/Test.java
@@ -5,7 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		
 		Dictionary.addResource("Test", 1);
-System.out.println(Dictionary.getValue("Test"));
+		System.out.println(Dictionary.getValue("Test"));
+		wr
 	}
 
 }
